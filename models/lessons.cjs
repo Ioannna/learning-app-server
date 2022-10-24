5846d4bfeb5b857712af9810af4d7de58dca3921
Ioannna <ioanabiiris@gmail.com>
2022-10-24 18:46:23 +0300

    get lessons from eLearning-try db

--- models/lessons.cjs
+++ models/lessons.cjs
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose")
 
-const lessonSchema = new mongoose.Schema({
-    lesson:{
-        type: String,
-        required: true,
-    }
+const partSchema = new mongoose.Schema({
+    name: String
 })
 
-const Lesson = mongoose.model('lesson', lessonSchema)
+const Part = mongoose.model('Part', partSchema)
 
+const quizSchema = new mongoose.Schema({
+    name: String
+})
+
+const Quiz = mongoose.model('Quiz', quizSchema)
+
+const lessonSchema = mongoose.Schema({
+    name: String,
+    category: String,
+    semester: String,
+    parts: [ partSchema ],
+    quiz: quizSchema
+})
+
+
+const Lesson = mongoose.model('Lesson', lessonSchema)
+
+module.exports = Part
+module.exports = Quiz
 module.exports = Lesson
--- server.cjs
+++ server.cjs
@@ -1,13 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Lesson = require('./models/lessons.cjs')
-const Grade = require('./models/grades.cjs')
+const Category = require('./models/categories.cjs')
+//const Semester = require('./models/categories.cjs')
+const Part = require('./models/lessons.cjs')
+const Quiz = require('./models/lessons.cjs')
 
-const mongoDB = 'mongodb+srv://eLearningUser:eLearning450@cluster0.mttrqxy.mongodb.net/learningContent-database2?retryWrites=true&w=majority'
+//const mongoDB = 'mongodb+srv://eLearningUser:eLearning450@cluster0.mttrqxy.mongodb.net/learningContent-database2?retryWrites=true&w=majority'
+mongoose.connect('mongodb://localhost/eLearning-try')
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  console.log('connected')
-}).catch(err => console.log(err))
+// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+//   console.log('connected')
+// }).catch(err => console.log(err))
 
 const app = express()
 const http = require('http')
@@ -28,36 +32,83 @@ app.get("/", (req, res) => {
     console.log(`Server started on port ${port}`);
   });
 
-  const Parts = [
-    ["Lectia 1 Partea 1", "Lectia 1 Partea 2", "Lectia 1 Quiz"],
-    ["Lectia 2 Partea 1", "Lectia 2 Partea 2", "Lectia 2 Quiz"],
-    ["Lectia 3 Partea 1", "Lectia 3 Partea 2", "Lectia 3 Quiz"]
-  ];
+const lessonsArray = []
 
-  io.on("connection", (socket) => {
+   io.on("connection", (socket) => {
 
-    Lesson.find().then(result => {
-      socket.emit('send-lessons', result)
-    })
+    async function listLessons() {
+      const lessons = await Lesson.find()
+          console.log(lessons)
+          socket.emit('send-lessons', lessons)
+          
+    }
 
-    Grade.find().then(result => {
-      console.log(result)
-    })
+    async function listCategories() {
+      const categories = await Category.find()
+          socket.emit('send-categories', categories)
+    }
+
+    listCategories()
+    listLessons()
 
       console.log(`[SOCKET CONNECTED] ${socket.id}`)
 
       socket.emit("connected", socket.id)
 
-      socket.on("enter-class", (className) => {
-          socket.emit("data", className)
+      socket.on("enter-class", (categoryName) => {
+          //listLessons(categoryName)
+          socket.emit("data", categoryName)
       })
 
       socket.on('displayLesson', () => {
         socket.emit('displayLessonS')
       })
 
-    socket.on("displayContent", ({ lessonNumber, partNumber }) => {
-      socket.emit("displayPartS", Parts[lessonNumber-1][partNumber-1])
-      socket.emit("currentInfo",{ lessonNumber: lessonNumber, partNumber: partNumber } )
+    socket.on("displayContent", (partContent) => {
+      socket.emit("displayPartS", partContent)
+      //socket.emit("currentInfo", partContent )
   })
-})
+ })
+
+//----------------------mongoose------------------
+
+//--------------------createLesson----------------
+
+// async function createLesson(name, category, semester, parts, quiz) {
+//   const lesson = new Lesson({
+//       name,
+//       category,
+//       semester,
+//       parts, 
+//       quiz
+//   })
+
+//   const result = await lesson.save()
+//   console.log(result)
+// }
+
+
+// createLesson("Lesson 3 S2", "Clasa a 9-a", "Semester 2", [
+//   new Part({ name: 'Part 1'}),
+//   new Part({ name: 'Part 2'})
+// ], 
+//   new Quiz({ name: 'Quiz Lesson 3 Sem 2 Cls 9'})
+
+// )
+
+//------------------createCategory---------------------
+
+// async function createCategory(name) {
+//   const category = new Category({
+//       name
+//   })
+
+//   const result = await category.save()
+//   console.log(result)
+// }
+
+
+// createCategory("Clasa a 12-a")
+
+
+//-------------------mongoose-finish-----------------
